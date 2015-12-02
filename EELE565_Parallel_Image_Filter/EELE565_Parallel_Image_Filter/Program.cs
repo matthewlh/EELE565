4f0b1776255e15770917bbfaca51fe3f114b4823
matthewlh <matthewlhandley@gmail.com>
2015-12-02 14:16:52 -0700

    Added VS project

---
+++ EELE565_Parallel_Image_Filter/EELE565_Parallel_Image_Filter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EELE565_Parallel_Image_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+    }
+}

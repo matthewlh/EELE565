4845892bd39b5283fcc18cf64a75982f7d787e6e
matthewlh <matthewlhandley@gmail.com>
2015-12-06 16:58:38 -0700

    Sequential filtering now working

--- EELE565_Parallel_Image_Filter/EELE565_Parallel_Image_Filter/Program.cs
+++ EELE565_Parallel_Image_Filter/EELE565_Parallel_Image_Filter/Program.cs
@@ -4,37 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace EELE565_Parallel_Image_Filter
 {
     class Program
     {
-        static Bitmap   bitmap_in,
+        static Bitmap bitmap_in,
                         bitmap_out;
 
-        static int  width, height,
-                    filterWidth, filterHeight;
+        static int width, height;
+
+        const int filterWidth = 5,
+                    filterHeight = 5;
+
+        const double k = 1.0 / 9.0;
+
+        static double[,] kernel =
+            {
+                {0.0, 0.0,   k, 0.0, 0.0},
+                {0.0, 0.0,   k, 0.0, 0.0},
+                {  k,   k,   k,   k,   k},
+                {0.0, 0.0,   k, 0.0, 0.0},
+                {0.0, 0.0,   k, 0.0, 0.0}
+            };
 
         static void Main(string[] args)
         {
-            /* Open image and set things up */ 
-            bitmap_in = new Bitmap("");
+            Console.WriteLine("Opening image and setting things up...");
+            bitmap_in = new Bitmap(@"../../Pluto-Wide-FINAL-9-17-15.jpg");
+            bitmap_in = new Bitmap(@"../../photo3.jpg");
             width = bitmap_in.Width;
             height = bitmap_in.Height;
 
             /* create output bitmap of same size */
             bitmap_out = new Bitmap(width, height);
 
+            /* test by filtering whole image sequentially */
+            Console.WriteLine("Sequentially filtering image...");
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            filter(0, 0, width, height);
+            stopwatch.Stop();
+
+            Console.WriteLine("Seqential Runtime: {0} seconds.", stopwatch.ElapsedMilliseconds / 1000.0);
+
+            /* save output bitmap */
+            Console.WriteLine("Saving output image");
+            bitmap_out.Save(@"../../output.jpg", bitmap_in.RawFormat);
             
         }
 
         static void filter(int start_x, int start_y, int end_x, int end_y)
         {
-            int x, y, //image coordinates
-                i, j; //filter coordinates
+            int x, y,   // image coordinates
+                fx, fy, // filter image coordinates
+                i, j;   // filter coordinates
 
-            byte[]  RGBin  = new byte[3] { 0.0, 0.0, 0.0 },
-                    RGBout = new byte[3] { 0.0, 0.0, 0.0 };
+            byte[]  RGBin  = new byte[3] { 0, 0, 0 },
+                    RGBout = new byte[3] { 0, 0, 0 };
 
             Color pixel;
                     
@@ -44,12 +71,6 @@ namespace EELE565_Parallel_Image_Filter
             {
                 for(y = start_y; y < end_y; y++)
                 {
-                    /* get input pixel in RGB array */
-                    pixel = bitmap_in.GetPixel(x, y);
-                    RGBin[0] = pixel.R;
-                    RGBin[1] = pixel.G;
-                    RGBin[2] = pixel.B;
-
                     /* reset output RGB array */
                     RGBout[0] = 0;
                     RGBout[1] = 0;
@@ -58,9 +79,19 @@ namespace EELE565_Parallel_Image_Filter
                     /* for each filter element */
                     for (i = 0; i < filterWidth; i++)
                     {
-                        for(j = 0; j < filterHeight; j++)
+                        for (j = 0; j < filterHeight; j++)
                         {
+                            /* determine pixel coordinate of input image corosponding to filter coordinate */
+                            fx = (x -  filterWidth / 2 + i + width ) % width;
+                            fy = (y - filterHeight / 2 + j + height) % height;
 
+                            /* get input pixel in RGB array */
+                            pixel = bitmap_in.GetPixel(fx, fy);
+
+                            /* crunch the numbers */
+                            RGBout[0] += (byte)(pixel.R * kernel[i, j]);
+                            RGBout[1] += (byte)(pixel.G * kernel[i, j]);
+                            RGBout[2] += (byte)(pixel.B * kernel[i, j]);
                         }
                     }
 

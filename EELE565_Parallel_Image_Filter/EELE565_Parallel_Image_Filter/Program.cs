868f7901a9b48c362254a6725f1863e22ef95cb5
matthewlh <matthewlhandley@gmail.com>
2015-12-07 13:59:54 -0700

    Revert "Working, but slow"
    This reverts commit 2809c4254db9dbd3d8c459ae634cf8a8c6dfa334.

--- EELE565_Parallel_Image_Filter/EELE565_Parallel_Image_Filter/Program.cs
+++ EELE565_Parallel_Image_Filter/EELE565_Parallel_Image_Filter/Program.cs
@@ -11,15 +11,12 @@ namespace EELE565_Parallel_Image_Filter
 {
     class Program
     {
-        const int numThreads = 8;
+        const int numThreads = 1;
 
         static Thread[] threadArr;
 
-        public static Bitmap    bitmapIn,
-                                bitmapOut;
-
-        static byte[,,] pixelArrIn,
-                        pixelArrOut;
+        public static Bitmap bitmap_in,
+                      bitmap_out;
 
         static int width, height;
 
@@ -55,28 +52,17 @@ namespace EELE565_Parallel_Image_Filter
         {
             /* local vars */
             int i;
-            
-            /* start timing */
-            Stopwatch stopwatch = Stopwatch.StartNew();
 
-            Console.WriteLine("Opening image...");
-            bitmapIn = new Bitmap(@"../../Pluto-Wide-FINAL-9-17-15.jpg");
-            //bitmapIn = new Bitmap(@"../../photo3.jpg");
+            Console.WriteLine("Opening image and setting things up...");
+            bitmap_in = new Bitmap(@"../../Pluto-Wide-FINAL-9-17-15.jpg");
+            //bitmap_in = new Bitmap(@"../../photo3.jpg");
+            width = bitmap_in.Width;
+            height = bitmap_in.Height;
 
-            Console.WriteLine("Setting parameters...");
-            width = bitmapIn.Width;
-            height = bitmapIn.Height;
-
-            pixelArrIn  = new byte[width, height, 3];
-            pixelArrOut = new byte[width, height, 3];
-            
-            bitmapOut = new Bitmap(width, height);
-
-            Console.WriteLine("Converting bitmap to array...");
-            loadBitmap();
+            /* create output bitmap of same size */
+            bitmap_out = new Bitmap(width, height);
 
             /* create threads */
-            Console.WriteLine("Spawning threads...");
             threadArr = new Thread[numThreads];
             for(i = 0; i < numThreads; i++)
             {
@@ -88,7 +74,12 @@ namespace EELE565_Parallel_Image_Filter
                 Console.WriteLine("Thread {0}: columns {1} through {2}", i, start_x, end_x);
 
                 threadArr[i] = new Thread(() => filter(start_x, 0, end_x, height));
+            }
 
+            /* start threads */            
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (i = 0; i < numThreads; i++)
+            {
                 threadArr[i].Start();
             }
 
@@ -97,97 +88,72 @@ namespace EELE565_Parallel_Image_Filter
             {
                 threadArr[i].Join();
             }
-
-            /* save output bitmap */
-            Console.WriteLine("Converting array to bitmap...");
-            saveBitmap();
-            Console.WriteLine("Saving output image...");
-            bitmapOut.Save(@"../../output.jpg", bitmapIn.RawFormat);
-
-            /* done timing things */
             stopwatch.Stop();
-            Console.WriteLine("Runtime: {0} seconds for {1} threads",
+
+            Console.WriteLine("Runtime: {0} seconds for {1} threads", 
                 stopwatch.ElapsedMilliseconds / 1000.0, numThreads);
 
+            /* save output bitmap */
+            Console.WriteLine("Saving output image");
+            bitmap_out.Save(@"../../output.jpg", bitmap_in.RawFormat);
+            
         }
 
         static void filter(int start_x, int start_y, int end_x, int end_y)
         {
             int x, y,   // image coordinates
                 fx, fy, // filter image coordinates
-                i, j;   // filter coordinates                    
+                i, j;   // filter coordinates
+
+            byte[]  RGBin  = new byte[3] { 0, 0, 0 },
+                    RGBout = new byte[3] { 0, 0, 0 };
+
+            Color pixel;
+                    
 
             /* for each pixel in the image */
             for(x = start_x; x < end_x; x++)
             {
                 for(y = start_y; y < end_y; y++)
                 {
-                    /* init output RGB array */
-                    pixelArrOut[x, y, 0] = 0;
-                    pixelArrOut[x, y, 1] = 0;
-                    pixelArrOut[x, y, 2] = 0;
+                    /* reset output RGB array */
+                    RGBout[0] = 0;
+                    RGBout[1] = 0;
+                    RGBout[2] = 0;
 
                     /* for each filter element */
                     for (i = 0; i < filterWidth; i++)
                     {
                         for (j = 0; j < filterHeight; j++)
                         {
-                            ///* check for zero in kernal */
-                            //if(kernel[i, j] == 0.0)
-                            //{
-                            //    continue;
-                            //}
+                            /* check for zero in kernal */
+                            if(0.0 == kernel[i, j])
+                            {
+                                continue;
+                            }
 
                             /* determine pixel coordinate of input image corosponding to filter coordinate */
                             fx = (x -  filterWidth / 2 + i + width ) % width;
                             fy = (y - filterHeight / 2 + j + height) % height;
 
+                            /* get input pixel in RGB array */
+                            pixel = bitmap_in.GetPixel(fx, fy);
+
                             /* crunch the numbers */
-                            pixelArrOut[x, y, 0] += (byte)(pixelArrIn[fx, fy, 0] * kernel[i, j]);
-                            pixelArrOut[x, y, 1] += (byte)(pixelArrIn[fx, fy, 1] * kernel[i, j]);
-                            pixelArrOut[x, y, 2] += (byte)(pixelArrIn[fx, fy, 2] * kernel[i, j]);
+                            RGBout[0] += (byte)(pixel.R * kernel[i, j]);
+                            RGBout[1] += (byte)(pixel.G * kernel[i, j]);
+                            RGBout[2] += (byte)(pixel.B * kernel[i, j]);
                         }
                     }
 
                     /* write new RGB value to output pixel */
-                    
+                    pixel = Color.FromArgb(RGBout[0], RGBout[1], RGBout[2]);
+                    bitmap_out.SetPixel(x, y, pixel);
 
                 }
             }
         }
 
-        static void loadBitmap()
-        {
-            int x, y;
-
-            /* for each pixel in the image */
-            for (x = 0; x < width; x++)
-            {
-                for (y = 0; y < height; y++)
-                {
-                    /* save RGB data from Bitmap */
-                    pixelArrIn[x, y, 0] = bitmapIn.GetPixel(x, y).R;
-                    pixelArrIn[x, y, 1] = bitmapIn.GetPixel(x, y).G;
-                    pixelArrIn[x, y, 2] = bitmapIn.GetPixel(x, y).B;
-                }
-            }
-        }
-
-        static void saveBitmap()
-        {
-            int x, y;
-            Color pixel;
-
-            /* for each pixel in the image */
-            for (x = 0; x < width; x++)
-            {
-                for (y = 0; y < height; y++)
-                {
-                    pixel = Color.FromArgb(pixelArrOut[x, y, 0], pixelArrOut[x, y, 1], pixelArrOut[x, y, 2]);
-                    bitmapOut.SetPixel(x, y, pixel);
-                }
-            }
-        }
 
     }
 }
